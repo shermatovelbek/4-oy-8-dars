ab946676a747aeb7f2a2f548fc02e8cfeb41cba7
shermatovelbek <shermatovelbek772@gmail.com>
2023-12-11 14:52:43 +0500

    misollar 6 gacha ishlanildi

--- main.js
+++ main.js
@@ -40,7 +40,6 @@
 
 // const arr = [1 , 2 , "salom" , false];
 
-
 //  sum = 0;
 // for(const item of arr){
 //     sum += item;
@@ -50,8 +49,7 @@
 // qiymatni ozgartirish
 // arr[2] = "hayr"
 
-
-//qiymat qo'shishlar 
+//qiymat qo'shishlar
 
 //ohiriga qo'shish
 //arr.push("xayr")
@@ -78,7 +76,7 @@
 // user.phone = "+998946749319"
 
 // console.log(user)
-  //                    Uyga vazifalar boshlandi !!!
+//                    Uyga vazifalar boshlandi !!!
 // 1-masala
 
 // function numbers(arr){
@@ -94,16 +92,74 @@
 
 //2-masala
 
-function sent(arg){
-    let arr = [];
-    for (const item of arg) {
-        arr += item.slice(0,2);
-    }
-    return arr
+// function sent(arg){
+//     let sum = [];
+//     for (const item of arg) {
+//         sum += item.slice(0,1);
+//     }
+//     return sum
+// }
+// let n = ["salom", "xayr"]
+// console.log(sent(n));            //domla bu misolda bow harflari ciqishi kerakmidi ?
+
+//3-masala
+
+// function sum1(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 == 1) {
+//       console.log(arr[i])
+
+//     }
+//   }
+//   return sum;
+// }
+// console.log(sum1([1, 2, 3, 4, 5, 7 , 9]));   //5
+
+//4-masala
+
+// function sum1(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 == 1) {
+//       sum++;
+//     }
+//   }
+//   return sum;
+// }
+// console.log(sum1([1, 2, 3, 4, 5, 7 , 9]));   //5
+
+//5-masala
+
+// function max(arr)
+// {
+//     return arr.filter(function(element) {
+//         return element.length > 5;
+//     });
+// }
+// const max2 = ["helo", "helloo"];
+
+// const sum = max(max2);
+
+// console.log(sum);
+
+// 6-misol
+
+function ex6(arg){
+     let sum = 0;
+     let res = 0;
+     for (const iterator of arg) {
+         for (let i = 1; i <= iterator; i++){
+             if (iterator % i == 0) {
+                 sum++;
+             }
+             if (sum == 2){
+                 res++;
+             }
+         }
+
+     }
+     return res
 }
-let n = ["salom", "xayr"]
-console.log(sent(n));
 
-
-
-
+console.log(ex6([1 , 7 , 9]));
